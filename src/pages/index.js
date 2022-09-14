d7002e80a745ac9e832ae9f4870a78cd267938ee
Sam Beevors <sambeevors@googlemail.com>
2022-09-14 12:36:20 +0100

    Added mailing list

--- src/pages/index.js
+++ src/pages/index.js
@@ -193,7 +193,7 @@ export default function Home() {
                 >
                   Preorder "Euphoria, Where Were You?"
                 </motion.a>
-                <div className="mt-16 uppercase">
+                <div className="my-16 uppercase">
                   <a
                     className="bit-widget-initializer"
                     data-artist-name="id_15509657"
@@ -281,6 +281,80 @@ export default function Home() {
                     data-optin-cta-border-color="undefined"
                   ></a>
                 </div>
+
+                {/* Begin Mailchimp Signup Form */}
+                <div id="mc_embed_signup">
+                  <form
+                    action="https://world.us12.list-manage.com/subscribe/post?u=5381817b945cf1d237f219562&id=9366de9b6a&f_id=003eb7e0f0"
+                    method="post"
+                    id="mc-embedded-subscribe-form"
+                    name="mc-embedded-subscribe-form"
+                    className="validate bg-black bg-opacity-50 p-12 mx-auto max-w-2xl"
+                    target="_self"
+                  >
+                    <div id="mc_embed_signup_scroll">
+                      <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl mb-2">
+                        Join the mailing list
+                      </h2>
+                      <div className="mc-field-group space-x-2">
+                        <label htmlFor="mce-EMAIL">Email Address</label>
+                        <input
+                          type="email"
+                          defaultValue=""
+                          name="EMAIL"
+                          className="required email bg-transparent border-2 border-white text-white p-2 min-w-[200px]"
+                          id="mce-EMAIL"
+                          required=""
+                        />
+                        <span
+                          id="mce-EMAIL-HELPERTEXT"
+                          className="helper_text"
+                        />
+                      </div>
+                      <div id="mce-responses" className="clear foot">
+                        <div
+                          className="response"
+                          id="mce-error-response"
+                          style={{ display: 'none' }}
+                        />
+                        <div
+                          className="response"
+                          id="mce-success-response"
+                          style={{ display: 'none' }}
+                        />
+                      </div>{' '}
+                      {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups*/}
+                      <div
+                        style={{ position: 'absolute', left: '-5000px' }}
+                        aria-hidden="true"
+                      >
+                        <input
+                          type="text"
+                          name="b_5381817b945cf1d237f219562_9366de9b6a"
+                          tabIndex={-1}
+                          defaultValue=""
+                        />
+                      </div>
+                      <div className="optionalParent">
+                        <div className="clear foot">
+                          <motion.input
+                            whileHover={{ backgroundColor: '#e22d2d' }}
+                            whileTap={{
+                              backgroundColor: '#e63939',
+                              scale: 0.95
+                            }}
+                            className="button mt-3 lg:mt-6 inline-flex items-center justify-center space-x-1 px-4 lg:px-6 py-3 bg-red-600 text-white leading-none lg:text-lg italic shadow"
+                            type="submit"
+                            defaultValue="Subscribe"
+                            name="subscribe"
+                            id="mc-embedded-subscribe"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                {/*End mc_embed_signup*/}
               </motion.div>
             </Countdown>
           </div>
